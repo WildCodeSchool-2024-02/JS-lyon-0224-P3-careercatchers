d5fe1e65e07d9a1dbad2b848f21b292e2c6791d8
Léo-Paul Montalbetti <montalbetti.lp@gmail.com>
2024-06-06 11:04:49 +0200

    test

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,12 +2,13 @@ import { useLoaderData, Link } from "react-router-dom";
 
 export default function HomePage() {
   const offers = useLoaderData();
+  // toto
   return (
     <>
       <h1>Externatic</h1>
       <p>Attrapez votre avenir</p>
       <p>{offers[0].job_title}</p>
-      <Link to="/result-page">Commencer ma recherche d'emploi</Link>
+      <Link to="/result-page">Commencer ma recherche d'emploi</Link> 
     </>
   );
 }
