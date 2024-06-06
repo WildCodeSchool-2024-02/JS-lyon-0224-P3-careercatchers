70303c04c9ec09bd895609e03ef4e28440b213dc
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-06-06 10:33:53 +0200

    creation LogoExternatic component

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,9 @@
+import LogoExternatic from "../components/LogoExternatic";
+
 export default function HomePage() {
   return (
     <>
+      <LogoExternatic />
       <h1>Externatic</h1>
       <p>Attrapez votre avenir</p>
     </>
