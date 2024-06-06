3e79ea98b67dc8b008f32b0ed9d1bb7795ff5728
Léo-Paul Montalbetti <montalbetti.lp@gmail.com>
2024-06-06 10:15:18 +0200

    changed the VARCHAR value for job_title and switched to ENUM for job_type

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE offer(
   id int unsigned primary key auto_increment not null,
-  job_title VARCHAR(255) NOT NULL,
-  job_type VARCHAR(255) NOT NULL
+  job_title VARCHAR(80) NOT NULL,
+  job_type ENUM("CDD","CDI","Alternance") NOT NULL
 );
 
