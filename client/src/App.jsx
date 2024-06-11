539f67c3143116bb1320592d8ff487b916da6ad2
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-06-11 13:25:38 +0200

    install tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,12 @@ import { Outlet } from "react-router-dom";
 import "./App.module.css";
 
 function App() {
-  return <Outlet />;
+  return (
+    <>
+      <Outlet />
+      <h2 className="bg-green-200">COUCOUC</h2>
+    </>
+  );
 }
 
 export default App;
