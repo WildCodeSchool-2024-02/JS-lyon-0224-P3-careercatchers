3d045fa73fc4f0cab7831a251da6453f73a04766
Léo-Paul Montalbetti <montalbetti.lp@gmail.com>
2024-06-12 16:55:47 +0200

    added a transition effect on the burger menu

--- client/src/components/BurgerMenu.jsx
+++ client/src/components/BurgerMenu.jsx
@@ -13,43 +13,41 @@ export default function BurgerMenu() {
 
   return (
     <div>
-      {isOpen === false && (
+      {!isOpen && (
         <button type="button" onClick={handleClick} className={styles.burger}>
           <img src={Burger} alt="open burger menu" />
         </button>
       )}
-      {isOpen === true && (
-        <div className={styles.menuOpen}>
-          <button type="button" onClick={handleClick} className={styles.cross}>
-            <img src={Cross} alt="close burger menu" />
-          </button>
-          <ul className={styles.ul}>
-            <Link to="/" className={styles.link} aria-label="HomePage">
-              <li>
-                Accueil<p>&#62;</p>
-              </li>
-            </Link>
-            <Link
-              to="/login-page"
-              className={styles.link}
-              aria-label="Log in to your account"
-            >
-              <li>
-                Se connecter<p>&#62;</p>
-              </li>
-            </Link>
-            <Link
-              to="sign-up-page"
-              className={styles.link}
-              aria-label="Create a new account"
-            >
-              <li>
-                S'inscrire<p>&#62;</p>
-              </li>
-            </Link>
-          </ul>
-        </div>
-      )}
+      <div className={`${styles.menuOpen} ${isOpen ? styles.active : ""}`}>
+        <button type="button" onClick={handleClick} className={styles.cross}>
+          <img src={Cross} alt="close burger menu" />
+        </button>
+        <ul className={styles.ul}>
+          <Link to="/" className={styles.link} aria-label="HomePage">
+            <li>
+              Accueil<p>&#62;</p>
+            </li>
+          </Link>
+          <Link
+            to="/login-page"
+            className={styles.link}
+            aria-label="Log in to your account"
+          >
+            <li>
+              Se connecter<p>&#62;</p>
+            </li>
+          </Link>
+          <Link
+            to="sign-up-page"
+            className={styles.link}
+            aria-label="Create a new account"
+          >
+            <li>
+              S'inscrire<p>&#62;</p>
+            </li>
+          </Link>
+        </ul>
+      </div>
     </div>
   );
 }
