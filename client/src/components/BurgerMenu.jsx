439a600e600012ca2fa0984a3283ee93b274cf81
Léo-Paul Montalbetti <montalbetti.lp@gmail.com>
2024-06-13 12:22:35 +0200

    made conditions more explicit, improved the accessibility of the burger menu with a lot of small improvements

--- client/src/components/BurgerMenu.jsx
+++ client/src/components/BurgerMenu.jsx
@@ -6,48 +6,71 @@ import Cross from "../assets/logo/x.svg";
 
 export default function BurgerMenu() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
   const handleClick = () => {
-    setIsOpen(!isOpen);
+    if (isOpen === true) {
+      setIsOpen(false);
+      setTimeout(() => {
+        setIsVisible(false);
+      }, 300);
+    } else {
+      setIsVisible(true);
+      setIsOpen(true);
+    }
   };
 
   return (
     <div>
-      {!isOpen && (
-        <button type="button" onClick={handleClick} className={styles.burger}>
+      {isOpen === false && (
+        <button
+          type="button"
+          onClick={handleClick}
+          className={styles.burger}
+          aria-expanded={isOpen}
+          aria-controls="burger-menu"
+        >
           <img src={Burger} alt="open burger menu" />
         </button>
       )}
-      <div className={`${styles.menuOpen} ${isOpen ? styles.active : ""}`}>
+      <nav
+        className={`${styles.menuOpen} ${isOpen === true ? styles.active : ""} ${isVisible === true ? "" : styles.hidden}`}
+        role="navigation"
+        aria-label="Main Menu"
+        id="burger-menu"
+      >
         <button type="button" onClick={handleClick} className={styles.cross}>
           <img src={Cross} alt="close burger menu" />
         </button>
         <ul className={styles.ul}>
-          <Link to="/" className={styles.link} aria-label="HomePage">
-            <li>
-              Accueil<p>&#62;</p>
-            </li>
-          </Link>
-          <Link
-            to="/login-page"
-            className={styles.link}
-            aria-label="Log in to your account"
-          >
-            <li>
-              Se connecter<p>&#62;</p>
-            </li>
-          </Link>
-          <Link
-            to="sign-up-page"
-            className={styles.link}
-            aria-label="Create a new account"
-          >
-            <li>
-              S'inscrire<p>&#62;</p>
-            </li>
-          </Link>
+          <li>
+            <Link to="/" className={styles.link} aria-label="HomePage">
+              Accueil
+            </Link>
+            <p>&#62;</p>
+          </li>
+          <li>
+            <Link
+              to="/login-page"
+              className={styles.link}
+              aria-label="Log in to your account"
+            >
+              Se connecter
+            </Link>
+            <p>&#62;</p>
+          </li>
+          <li>
+            <Link
+              to="sign-up-page"
+              className={styles.link}
+              aria-label="Create a new account"
+            >
+              S'inscrire
+            </Link>
+            <p>&#62;</p>
+          </li>
         </ul>
-      </div>
+      </nav>
     </div>
   );
 }
