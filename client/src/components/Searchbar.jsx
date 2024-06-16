d1144a2df517a9bc6e58082d575e0aef9cddbfbb
Bastien Fournier <bastien.fournier06@gmail.com>
2024-06-16 13:26:24 +0200

    update search bar

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -2,11 +2,14 @@ import Search from "../assets/logo/search.svg";
 
 export default function SearchBar() {
   return (
-    <div className="flex justify-center mr-10 ml-10 border ">
-      <input type="text" placeholder="Domaine, poste..." />
-      <input type="text" placeholder="Ville" />
-      <button type="button">
-        <img className="h-9" src={Search} alt="logo loupe" />
+    <div className="flex justify-between mr-10 ml-10 border-solid border-2 rounded-md ">
+      <input
+        className="flex outline-none"
+        type="text"
+        placeholder="Domaine, poste..."
+      />
+      <button className="bg-pink-600  rounded-md" type="button">
+        <img className="h-8" src={Search} alt="logo loupe" />
       </button>
     </div>
   );
