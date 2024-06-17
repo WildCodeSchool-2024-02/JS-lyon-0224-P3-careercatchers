8aa822fa2a50b429ab40409e5d78c63f2e52a005
Bastien Fournier <bastien.fournier06@gmail.com>
2024-06-17 12:13:11 +0200

    css serach bar

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -2,14 +2,14 @@ import Search from "../assets/logo/search.svg";
 
 export default function SearchBar() {
   return (
-    <div className="flex justify-between mr-10 ml-10 border-solid border-2 rounded-md ">
+    <div className="flex ml-10 ">
       <input
-        className="flex outline-none"
+        className="flex outline-none border-solid border-2 rounded-s-md w-4/5 border-r-0"
         type="text"
-        placeholder="Domaine, poste..."
+        placeholder=" Domaine, poste..."
       />
-      <button className="bg-pink-600  rounded-md" type="button">
-        <img className="h-8" src={Search} alt="logo loupe" />
+      <button className="bg-primary  rounded-e-md" type="button">
+        <img className="h-8" src={Search} alt="lancer la recherche" />
       </button>
     </div>
   );
