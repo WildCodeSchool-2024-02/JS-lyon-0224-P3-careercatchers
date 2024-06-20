87958df31f75bde939a9c9e7750ca3ab290d8b60
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-06-20 15:29:39 +0200

    creation du state errors pour chaque input

--- client/src/components/Form/SignUpForm.jsx
+++ client/src/components/Form/SignUpForm.jsx
@@ -3,21 +3,52 @@ import { Form } from "react-router-dom";
 
 function SignUpForm() {
   const [register, setRegister] = useState({
-    email: "",
-    password: "",
     lastname: "",
     firstname: "",
+    email: "",
     birthday: "",
+    sex: "",
+    password: "",
+    confirmPassword: "",
   });
+  const [errors, setErrors] = useState({});
 
   const handleUpdateForm = (e) => {
     const { name, value } = e.target;
     setRegister({ ...register, [name]: value });
+    setErrors({ ...errors, [name]: "" });
   };
 
+  const validateForm = () => {
+    const newErrors = {};
+
+    if (register.firstname === "")
+      newErrors.firstname = "Le prénom est requis.";
+    if (register.lastname === "") newErrors.lastname = "Le nom est requis.";
+    if (register.email === "") newErrors.email = "L'e-mail est requis.";
+    if (register.birthday === "")
+      newErrors.birthday = "La date de naissance est requise.";
+    if (register.sex === "") newErrors.sex = "Le sexe est requis.";
+    if (register.password === "")
+      newErrors.password = "Le mot de passe est requis.";
+    if (register.password !== register.confirmPassword)
+      newErrors.confirmPassword = "Les mots de passe ne correspondent pas.";
+
+    setErrors(newErrors);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      console.info("Formulaire soumis avec succès !", register);
+    }
+  };
+
+  const inputStyles =
+    "w-full px-3 py-2 border rounded focus:outline-none focus:border-secondary focus:ring focus:ring-primary focus:ring-opacity-20";
   return (
     <div className=" flex flex-col w-5/6 mx-auto max-w-sm ">
-      <Form method="post">
+      <Form method="post" onSubmit={handleSubmit}>
         <div className="my-4 min-w-96">
           <h3 className=" text-xl font-custom mx-2 my-6">Je suis :</h3>
           <div className="flex justify-evenly">
@@ -41,13 +72,18 @@ function SignUpForm() {
           </label>
           <input
             aria-required="true"
+            aria-describedby="firstNameDescribe"
             type="text"
             id="firstname"
             name="firstname"
             value={register.firstname}
             onChange={handleUpdateForm}
-            className="w-full px-3 py-2 border rounded focus:outline-none focus:border-secondary focus:ring focus:ring-primary focus:ring-opacity-20"
+            className={`${inputStyles} ${errors.firstname ? "border-red-500" : ""}`}
           />
+
+          {errors.firstname && (
+            <p className="text-red-500">{errors.firstname}</p>
+          )}
         </div>
         <div className="mb-4">
           <label htmlFor="lastname" className="block text-gray-500">
@@ -55,13 +91,17 @@ function SignUpForm() {
           </label>
           <input
             aria-required="true"
+            aria-describedby="lastnameDescribe"
             type="text"
             id="lastname"
             name="lastname"
             value={register.lastname}
             onChange={handleUpdateForm}
-            className="w-full px-3 py-2 border rounded focus:outline-none focus:border-secondary focus:ring focus:ring-primary focus:ring-opacity-20"
+            className={`${inputStyles} ${
+              errors.lastname ? "border-red-500" : ""
+            }`}
           />
+          {errors.lastname && <p className="text-red-500">{errors.lastname}</p>}
         </div>
         <div className="mb-4">
           <label htmlFor="email" className="block text-gray-500">
@@ -69,13 +109,15 @@ function SignUpForm() {
           </label>
           <input
             aria-required="true"
+            aria-describedby="emailDescribe"
             type="email"
             id="email"
             name="email"
             value={register.email}
             onChange={handleUpdateForm}
-            className="w-full px-3 py-2 border rounded focus:outline-none focus:border-secondary focus:ring focus:ring-primary focus:ring-opacity-20"
+            className={`${inputStyles}${errors.email ? "border-red-500" : ""}`}
           />
+          {errors.email && <p className="text-red-500">{errors.email}</p>}
         </div>
         <div className="mb-4">
           <label htmlFor="birthday" className="block text-gray-500">
@@ -83,44 +125,53 @@ function SignUpForm() {
           </label>
           <input
             aria-required="true"
+            aria-describedby="birthdayDescribe"
             type="date"
             id="birthday"
             name="birthday"
             value={register.birthday}
             onChange={handleUpdateForm}
-            className="w-full px-3 py-2 border rounded focus:outline-none focus:border-secondary focus:ring focus:ring-primary focus:ring-opacity-20"
+            className={`${inputStyles}${
+              errors.birthday ? "border-red-500" : ""
+            }`}
           />
         </div>
+        {errors.birthday && <p className="text-red-500">{errors.birthday}</p>}
         <div className="mb-4">
           <label htmlFor="sex" className="block text-gray-500">
             Sexe
           </label>
           <select
             aria-required="true"
+            aria-describedby="sexDescribe"
             id="sex"
             name="sex"
             value={register.sex}
             onChange={handleUpdateForm}
-            className="w-full px-3 py-2 border rounded focus:outline-none focus:border-secondary focus:ring focus:ring-primary focus:ring-opacity-20"
+            className={`${inputStyles} ${errors.sex ? "border-red-500" : ""}`}
           >
+            <option value="femme">{}</option>
             <option value="femme">Féminin</option>
             <option value="homme">Masculin</option>
           </select>
+          {errors.sex && <p className="text-red-500">{errors.sex}</p>}
         </div>
         <div className="mb-4">
           <label htmlFor="password" className="block text-gray-500">
             Mot de passe
           </label>
           <input
-            autoComplete="new-password"
+            autoComplete="password"
             aria-required="true"
+            aria-describedby="passwordDescribe"
             type="password"
             id="password"
             name="password"
             value={register.password}
             onChange={handleUpdateForm}
-            className="w-full px-3 py-2 border rounded focus:outline-none focus:border-secondary focus:ring focus:ring-primary focus:ring-opacity-20"
+            className={`${inputStyles}  ${errors.password ? "border-red-500" : ""}`}
           />
+          {errors.password && <p className="text-red-500">{errors.password}</p>}
         </div>
         <div className="mb-4">
           <label htmlFor="confirmPassword" className="block text-gray-500">
@@ -128,13 +179,19 @@ function SignUpForm() {
           </label>
           <input
             aria-required="true"
+            aria-describedby="confirmPasswordDescribe"
             type="password"
             id="confirmPassword"
             name="confirmPassword"
             value={register.confirmPassword}
             onChange={handleUpdateForm}
-            className="w-full px-3 py-2 border rounded focus:outline-none focus:border-secondary focus:ring focus:ring-primary focus:ring-opacity-20"
+            className={`${inputStyles}
+              ${errors.confirmPassword ? "border-red-500" : ""}`}
           />
+
+          {errors.confirmPassword && (
+            <p className="text-red-500">{errors.confirmPassword}</p>
+          )}
         </div>
         <div className="flex justify-end">
           <button
