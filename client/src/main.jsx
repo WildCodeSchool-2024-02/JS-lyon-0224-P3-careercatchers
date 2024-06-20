01cfd89627ba499c41953c272259ea89b0a768b2
Léo-Paul Montalbetti <montalbetti.lp@gmail.com>
2024-06-20 16:17:13 +0200

    prettier

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 
 import App from "./App";
 import signUpAction from "./components/Form/actionSignUp";
