db7b49aa1f73a1da010c028dc6870c1aba024e14
Bastien Fournier <bastien.fournier06@gmail.com>
2024-06-20 12:07:31 +0200

    map in jobOffer components from table offer

--- client/src/components/JobOffer/JobOffer.jsx
+++ client/src/components/JobOffer/JobOffer.jsx
@@ -1,25 +1,32 @@
+import { useLoaderData } from "react-router-dom";
+
 import Heart from "../../assets/logo/heart.svg";
 import styles from "./JobOffer.module.css";
 
 export default function JobOffer() {
+  const offers = useLoaderData();
   return (
-    <div className="flex justify-center">
-      <div className={styles.divJobOffer}>
-        <div className={styles.head}>
-          <h2 className={styles.title}>Titre poste</h2>
-          <img
-            className={styles.logoHeart}
-            src={Heart}
-            alt="Ajouter l'offre aux favoris"
-          />
+    <div className="flex  ">
+      {offers.map((offer) => (
+        <div key={offer.id} className={styles.divJobOffer}>
+          <div className={styles.head}>
+            <h2 className={styles.title}>{offer.job_title}</h2>
+            <img
+              className={styles.logoHeart}
+              src={Heart}
+              alt="Ajouter l'offre aux favoris"
+            />
+          </div>
+          <p className={styles.adress}>
+            {offer.localisation} {offer.name}
+          </p>
+          <p className={styles.jobSalary}>coucou</p>
+          <p className={styles.contract}>{offer.job_type}</p>
+          <div className={styles.date}>
+            <p>Date de mise en ligne de l'offre</p>
+          </div>
         </div>
-        <p className={styles.adress}>Adresse entreprise</p>
-        <p className={styles.jobSalary}>Salaire proposé entreprise</p>
-        <p className={styles.contract}>Type de contrat</p>
-        <div className={styles.date}>
-          <p>Date de mise en ligne de l'offre</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
