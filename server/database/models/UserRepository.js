72238b10887fc8c8b23417871e6686a61f2051cc
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-06-26 12:04:16 +0200

    in UserRepository insert hasedPassword in user table

--- server/database/models/UserRepository.js
+++ server/database/models/UserRepository.js
@@ -13,10 +13,10 @@ class UserRepository extends AbstractRepository {
   async create(user) {
     const [result] = await this.database.query(
       `
-      INSERT INTO ${this.table} (email, password)
+      INSERT INTO ${this.table} (email, hashed_password)
       VALUES (?, ?)
     `,
-      [user.email, user.password]
+      [user.email, user.hashedPassword]
     );
 
     // Execute the query and return the result
