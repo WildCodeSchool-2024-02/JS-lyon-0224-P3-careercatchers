f118b98d507f3212ac38cd374e405ae301688716
Léo-Paul Montalbetti <montalbetti.lp@gmail.com>
2024-06-26 11:29:20 +0200

    submitting the form to post an offer now displays a toast with either a success or fail message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
 import { Outlet } from "react-router-dom";
 import "./App.module.css";
 import "./index.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return <Outlet />;
+  return (
+    <main>
+      <Outlet />
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </main>
+  );
 }
 
 export default App;
