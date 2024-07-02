b95499387af7c63ee30c93a27f7f7126242b9437
Bastien Fournier <bastien.fournier06@gmail.com>
2024-07-02 16:41:52 +0200

    put map in reslut page components

--- client/src/components/JobOffer/JobOffer.jsx
+++ client/src/components/JobOffer/JobOffer.jsx
@@ -1,33 +1,45 @@
-import { useLoaderData } from "react-router-dom";
+import PropTypes from "prop-types";
 
 import Heart from "../../assets/logo/heart.svg";
 import styles from "./JobOffer.module.css";
 
-export default function JobOffer() {
-  const offers = useLoaderData();
-
+export default function JobOffer({
+  jobTitle,
+  name,
+  location,
+  minSalary,
+  maxSalary,
+  jobType,
+}) {
   return (
     <div className="flex flex-col items-center mt-20">
-      {offers.map((offer) => (
-        <div key={offer.id} className={styles.divJobOffer}>
-          <div className={styles.head}>
-            <h2 className={styles.title}>{offer.job_title}</h2>
-            <img
-              className={styles.logoHeart}
-              src={Heart}
-              alt="Ajouter l'offre aux favoris"
-            />
-          </div>
-          <p className={styles.name}>{offer.name}</p>
-          <p className={styles.adress}>{offer.location}</p>
-          {(offer.min_salary !== null || offer.max_salary !== null) && (
-            <p className={styles.jobSalary}>
-              {offer.min_salary} - {offer.max_salary}
-            </p>
-          )}
-          <p className={styles.contract}>{offer.job_type}</p>
+      <div className={styles.divJobOffer}>
+        <div className={styles.head}>
+          <h2 className={styles.title}>{jobTitle}</h2>
+          <img
+            className={styles.logoHeart}
+            src={Heart}
+            alt="Ajouter l'offre aux favoris"
+          />
         </div>
-      ))}
+        <p className={styles.name}>{name}</p>
+        <p className={styles.adress}>{location}</p>
+        {(minSalary !== null || maxSalary !== null) && (
+          <p className={styles.jobSalary}>
+            {minSalary} - {maxSalary}
+          </p>
+        )}
+        <p className={styles.contract}>{jobType}</p>
+      </div>
     </div>
   );
 }
+
+JobOffer.propTypes = {
+  jobTitle: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  minSalary: PropTypes.number.isRequired,
+  maxSalary: PropTypes.number.isRequired,
+  jobType: PropTypes.string.isRequired,
+};
--- client/src/pages/ResultPage.jsx
+++ client/src/pages/ResultPage.jsx
@@ -1,13 +1,27 @@
+import { useLoaderData } from "react-router-dom";
+
 import JobOffer from "../components/JobOffer/JobOffer";
 import NavBarMobile from "../components/NavBar/NavBarMobile";
 import SearchBar from "../components/SearchBar/Searchbar";
 
 export default function ResultPage() {
+  const offers = useLoaderData();
+
   return (
     <>
       <NavBarMobile />
       <SearchBar />
-      <JobOffer />
+      {offers.map((offer) => (
+        <JobOffer
+          key={offer.id}
+          jobTitle={offer.job_title}
+          name={offer.name}
+          location={offer.location}
+          minSalary={offer.min_salary}
+          maxSalary={offer.max_salary}
+          jobType={offer.job_type}
+        />
+      ))}
     </>
   );
 }
