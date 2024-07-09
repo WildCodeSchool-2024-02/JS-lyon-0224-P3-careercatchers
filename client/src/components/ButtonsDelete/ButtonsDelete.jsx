e783db70143159050012185dd8af780793577fdd
Bastien Fournier <bastien.fournier06@gmail.com>
2024-07-09 15:16:09 +0200

    fix bug

--- client/src/components/ButtonsDelete/ButtonsDelete.jsx
+++ client/src/components/ButtonsDelete/ButtonsDelete.jsx
@@ -37,12 +37,6 @@ export default function ButtonsDelete({ id }) {
       >
         Supprimer
       </button>
-      <button
-        className="flex bg-primary justify-center font-custom text-white rounded-md mx-auto max-w-sm min-w-60 px-3 py-2 mb-1 border"
-        type="button"
-      >
-        Modifier
-      </button>
     </div>
   );
 }
