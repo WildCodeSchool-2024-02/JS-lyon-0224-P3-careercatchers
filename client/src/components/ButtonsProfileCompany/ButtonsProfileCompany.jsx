0578326791065f35749ad77a4ab1cab20a687305
Léo-Paul Montalbetti <montalbetti.lp@gmail.com>
2024-07-11 15:55:32 +0200

    added a Link to the page PostNewOffer

--- client/src/components/ButtonsProfileCompany/ButtonsProfileCompany.jsx
+++ client/src/components/ButtonsProfileCompany/ButtonsProfileCompany.jsx
@@ -6,25 +6,36 @@ import Folder from "../../assets/logo/folder.svg";
 export default function ProfilPageCandidate() {
   return (
     <div className="flex flex-col  items-center gap-5 pt-14 ">
-      <Link to="/mes-offres">
+      <Link to="/mes-offres" tabIndex="-1">
         <button
           className="flex items-center bg-primary font-custom text-white rounded-md mx-auto max-w-sm min-w-60 px-3 py-2 mb-1 border relative"
           type="button"
         >
-          <img className="w-6 mr-3" src={Folder} alt="Logo favoris" />
+          <img className="w-6 mr-3" src={Folder} alt="Dossier" />
           <span className="absolute left-1/2 transform -translate-x-1/2">
             Mes infos
           </span>
         </button>
       </Link>
-      <Link to="/mes-candidats">
+      <Link to="/mes-candidats" tabIndex="-1">
         <button
           className="flex items-center bg-primary font-custom text-white rounded-md mx-auto max-w-sm min-w-60 px-3 py-2 mb-1 border relative"
           type="button"
         >
-          <img className="w-6 mr-3" src={Briefcase} alt="Logo favoris" />
+          <img className="w-6 mr-3" src={Briefcase} alt="Attaché-case" />
           <span className="absolute left-1/2 transform -translate-x-1/2">
             Mes candidatures
+          </span>
+        </button>
+      </Link>
+      <Link to="/post-offer" tabIndex="-1">
+        <button
+          className="flex items-center bg-primary font-custom text-white rounded-md mx-auto max-w-sm min-w-60 px-3 py-2 mb-1 border relative"
+          type="button"
+        >
+          <img className="w-6 mr-3" src={Briefcase} alt="Attaché-case" />
+          <span className="absolute left-1/2 transform -translate-x-1/2">
+            Poster une offre
           </span>
         </button>
       </Link>
