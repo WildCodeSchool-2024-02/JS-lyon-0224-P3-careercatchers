cda034e08cdaec43e513c3752da72211bedeacac
Léo-Paul Montalbetti <montalbetti.lp@gmail.com>
2024-07-12 12:28:30 +0200

    HomePage is now responsive with 2 breakpoints

--- client/src/components/HomeButtons/HomeButtons.jsx
+++ client/src/components/HomeButtons/HomeButtons.jsx
@@ -1,24 +1,35 @@
 import { Link } from "react-router-dom";
 import styles from "./HomeButtons.module.css";
-
+import { useUserContext } from "../../contexts/UserContext";
 import SignUpButton from "../SignUpButton/SignUpButton";
 
 export default function HomeButtons() {
-  return (
+  const { user } = useUserContext();
+  return user === null || user === "null" ? (
     <div className={styles.homeButtons}>
-      <Link to="/result-page" tabIndex="-1">
-        <button className={styles.buttons} type="button">
-          Commencer
-        </button>
-      </Link>
-      <Link to="/login-page" tabIndex="-1">
-        <button className={styles.buttons} type="button">
-          Me connecter
-        </button>
-      </Link>
+      <div className="flex justify-around w-3/6">
+        <Link to="/result-page" tabIndex="-1">
+          <button className={styles.buttons} type="button">
+            Commencer
+          </button>
+        </Link>
+        <Link to="/login-page" tabIndex="-1">
+          <button className={styles.buttons} type="button">
+            Me connecter
+          </button>
+        </Link>
+      </div>
       <div className={styles.divSignUp}>
         <SignUpButton />
       </div>
+    </div>
+  ) : (
+    <div className="flex justify-center mb-4">
+      <Link to="/result-page" tabIndex="-1">
+        <button className={styles.buttons} type="button">
+          Parcourir les offres
+        </button>
+      </Link>
     </div>
   );
 }
